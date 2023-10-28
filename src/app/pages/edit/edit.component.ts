f268dc8532917462b35c92b854b57dfac1b53b2a
Ysla Souza <yslasouzagnr@gmail.com>
2023-10-28 00:03:42 -0300

    Conclusão do fluxo de READ

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router'; // Importe o Router da biblioteca @angular/router
+import { ActivatedRoute, Router } from '@angular/router';
+import axios from 'axios';
 
 interface IParams {
   code: string;
@@ -41,14 +42,28 @@ export class EditComponent {
     if (this.code === '' || !this.code || this.code.length < 8) this.errorMessage = 'Necessário fornecer um código para o produto de pelo menos 8 caracteres';
     if (this.name === '' || !this.name || this.name.length < 2) this.errorMessage = 'Necessário fornecer um nome para o produto de pelo menos dois caracteres';
     if (this.errorMessage === '') {
-      console.log('cadastro realizado');
-      this.name = '';
-      this.code = '';
-      this.price = 0;
-      this.errorMessage = 'Produto Atualizado com sucesso!';
-      setTimeout(() => {
-        this.errorMessage = '';
-      }, 3000)
+      try {
+        this.errorMessage = 'Aguarde, estamos processando seu cadastro...';
+        await axios.post('http://localhost:8080/api/produtos', {
+          codigoBarras: this.code,
+          preco: this.price,
+          nome: this.name,
+        });
+        this.name = '';
+        this.code = '';
+        this.price = 0;
+        this.errorMessage = 'Produto Cadastrado com sucesso!';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 3000);
+      } catch(error: unknown) {
+        if (error instanceof Error && 'message' in error) {
+          const customError = error as Error;
+          this.errorMessage = `Ocorreu um erro ao tentar realizar o cadastro do Produto: ${customError.message}`;
+        } else {
+          this.errorMessage = 'Erro desconhecido';
+        }
+      }
     }
   }
 }
--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import axios from 'axios';
 
 interface IProduct {
-  name: string;
-  code: string;
-  price: number;
+  nome: string;
+  codigoBarras: string;
+  preco: number;
 }
 
 @Component({
@@ -18,37 +18,28 @@ export class ListComponent {
   deleteConfirmationVisible: boolean;
 
   constructor() {
-    this.list = [
-      { name: 'Produto 1', code: '123456789', price: 23.00 },
-      { name: 'Produto 2', code: '987654321', price: 22.00 },
-      { name: 'Produto 3', code: '246810121', price: 23.00 },
-      { name: 'Produto 4', code: '135791113', price: 24.85 },
-      { name: 'Produto 5', code: '510152025', price: 35.00 },
-      { name: 'Produto 1', code: '123456789', price: 23.00 },
-      { name: 'Produto 2', code: '987654321', price: 22.00 },
-      { name: 'Produto 3', code: '246810121', price: 23.00 },
-    ];
-    this.itemToDelete = { name: '', code: '', price: 0 };
+    this.list = [];
+    this.itemToDelete = { nome: '', codigoBarras: '', preco: 0 };
     this.deleteConfirmationVisible = false;
   };
 
   async ngOnInit() {
     const list = await axios.get('http://localhost:8080/api/produtos');
-    console.log(list);
+    this.list = list.data;
   };
 
   deleteItem(product: IProduct): void {
-    this.itemToDelete = { name: product.name, code: product.code, price: product.price };
+    this.itemToDelete = { nome: product.nome, codigoBarras: product.codigoBarras, preco: product.preco };
     this.deleteConfirmationVisible = true;
   };
 
   confirmDelete(): void {
     console.log("executa a exclusão");
-    this.itemToDelete = { name: '', code: '', price: 0 };
+    this.itemToDelete = { nome: '', codigoBarras: '', preco: 0 };
     this.deleteConfirmationVisible = false;
   };
   cancelDelete(): void {
-    this.itemToDelete = { name: '', code: '', price: 0 };
+    this.itemToDelete = { nome: '', codigoBarras: '', preco: 0 };
     this.deleteConfirmationVisible = false;
   };
 }
